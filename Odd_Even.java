3c182821e42c4937c24dd88c38de9f0a7cfd2101
Alfisha Ansari <ansarialfisha2006@gmail.com>
2025-07-12 21:12:45 +0530

    If - Else

---
+++ Odd_Even.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Odd_Even {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter number x : ");
+        int x = sc.nextInt();
+
+        if(x%2 == 0){
+            System.out.println("Number is Even");
+        } else {
+            System.out.println("Number is Odd");
+        }
+        
+         sc.close();
+    }
+    
+}
