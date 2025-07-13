0d1b02e6abf71d87bec0d22c227967d9712ac31c
Alfisha Ansari <ansarialfisha2006@gmail.com>
2025-07-13 23:29:47 +0530

    Nested Loops

---
+++ Patterns/Pattern7.java
@@ -0,0 +1,20 @@
+// Print this pattern
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i=n ; i>=1 ; i--){
+            for(int j=1 ; j<i+1 ; j++) {
+                System.out.print(j + " ");
+                }
+            System.out.println();
+            }
+            
+        }   
+    }
