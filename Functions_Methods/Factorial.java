5eb7c8897572044d711a223c1fe5060ba9c17abc
Alfisha Ansari <ansarialfisha2006@gmail.com>
2025-07-16 22:11:44 +0530

    Core Functions

---
+++ Functions_Methods/Factorial.java
@@ -0,0 +1,28 @@
+package Functions_Methods;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void calculateFact(int n) {
+       
+       int factorial = 1;
+       if (n <= 0){
+        System.out.println("Invalid");
+        return;
+       } else{
+        for(int i = 1; i<=n; i++){
+           factorial = factorial * i;
+            }   
+        System.out.println("Factorial of" + " " + n + " " + "is" + " " + factorial + ".");
+        return;
+       }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number n : ");
+        int n = sc.nextInt();
+        
+        calculateFact(n);
+    }
+}
---
+++ Functions_Methods/Functions.java
@@ -0,0 +1,17 @@
+package Functions_Methods;
+
+import java.util.Scanner;
+
+public class Functions {
+    public static void printMyName(String name) {
+        System.out.println(name);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name : ");
+        String name = sc.next();
+        
+        printMyName(name);
+    }
+}
